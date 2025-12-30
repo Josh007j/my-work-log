e2d9ccb8eea6547b3b9af6006452422d8a489cce
Josh007j <152661710+Josh007j@users.noreply.github.com>
2025-12-30 14:14:16 -0800

    Add files via upload
    initial portfolio launch

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'arome-v2';
+const assets = [
+  './',
+  './index.html',
+  './logo.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch events
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
